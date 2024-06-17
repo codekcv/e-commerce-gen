a47579dd6ea7491b03a5c34681b3a2989d1838a6
codekcv <ChristianVillamin31@gmail.com>
2024-06-18 05:47:13 +0800

    rmv posts related

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,8 @@
-import PostButton from "@/components/post-button";
 import { Button } from "@/components/ui/button";
-import createPost from "@/server/actions/create-post";
-import getPosts from "@/server/actions/get-posts";
 
 export default async function Home() {
-  const { error, success } = await getPosts();
-
-  if (error) {
-    throw new Error(error);
-  }
-
   return (
     <main>
-      {success?.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-        </div>
-      ))}
-
-      <form action={createPost}>
-        <input
-          className="bg-black"
-          type="text"
-          name="title"
-          placeholder="Title"
-        />
-        <PostButton />
-      </form>
-
       <h1>Welcome to Next.js</h1>
       <Button variant="destructive">Button</Button>
     </main>
